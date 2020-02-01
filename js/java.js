468cca07ffab38b9a0d9e7425b1eb8f5d46708bd
Diego Izidorio <d.izidorio14@outlook.com>
2020-02-01 02:10:03 -0300

    Alteração no botão topo, adição de do slideshow

--- js/java.js
+++ js/java.js
@@ -108,3 +108,14 @@ $(document).ready(function() {
         _containerPB.css({ display: "table" });;
     });
 });
+
+// SlideShow
+
+$(function() {
+    $('#ei-slider').eislideshow({
+        animation: 'center',
+        autoplay: true,
+        slideshow_interval: 3000,
+        titlesFactor: 0
+    });
+});
