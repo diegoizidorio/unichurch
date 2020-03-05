90c5e8eeafbbfe7e25f0fee35f837050c32e491a
Diego Izidorio <d.izidorio14@outlook.com>
2020-03-05 03:28:56 -0300

    Alteração na parte da oração

--- js/java.js
+++ js/java.js
@@ -119,3 +119,13 @@ $(function() {
         titlesFactor: 0
     });
 });
+
+//Popup
+
+function abrir() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+function fechar() {
+    document.getElementById('popup').style.display = 'none';
+}
